978d9bacafc32d7245f734fc26d4a6dbb863fcff
demichele-c <demichele.charles@yahoo.com>
2024-08-01 12:03:54 -0400

    added notes to sql

--- db/query.sql
+++ db/query.sql
@@ -1,3 +1,11 @@
+-- Query to retrieve all records from the department table
+-- This will fetch details for every department in the database
 SELECT * FROM department;
+
+-- Query to retrieve all records from the role table
+-- This will fetch details for every role (job position) in the database
 SELECT * FROM role;
+
+-- Query to retrieve all records from the employee table
+-- This will fetch details for every employee in the database
 SELECT * FROM employee;
